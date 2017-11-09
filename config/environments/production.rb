e08b833a7975d8dc8bb031ea23427eba63918b85
Mizuki Zenta <mizukisb@gmail.com>
2017-11-09 18:13:18 +1100

    'production-setting'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,13 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "rails-project_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.default_url_options = { host: 'mottainai-project.herokuapp.com' }
 
+  config.action_mailer.mailgun_settings = {
+   api_key: ENV.fetch('MILGUN_API_KEY'),
+   domain: ENV.fetch('MILGUN_DOMAIN'),
+ }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
