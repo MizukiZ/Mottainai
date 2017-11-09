690d4c7bc694a19edbd18342b31d889ef8165e18
Mizuki Zenta <mizukisb@gmail.com>
2017-11-10 02:06:07 +1100

    'seed-demo'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+User.last.destory
+Category.destroy_all
 
  # for categories
 Category.create(title:'Food')
